072e8640a93e5db3cfe61ab2240559a040dd4949
Ayaan Hussain <ayaanh@cmu.edu>
2021-03-14 20:28:19 -0400

    added

---
+++ front/src/views/Processing.js
@@ -0,0 +1,12 @@
+import AxiosCurlirize from 'axios-curlirize';
+import Async from 'react-async';
+export default function Processing() {
+
+  return (
+    <div>
+      <ul>
+        bruh
+      </ul>
+    </div>
+  );
+}
