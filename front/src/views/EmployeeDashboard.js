f17f5a5cbe90b83ee4dbd25fab5e3c55ce63f245
Aditya Chanana <achanana@andrew.cmu.edu>
2021-03-17 22:30:57 -0400

    Add ability to mark orders as complete

--- front/src/views/EmployeeDashboard.js
+++ front/src/views/EmployeeDashboard.js
@@ -1,17 +1,30 @@
+import { TextField } from '@material-ui/core';
 import { DataGrid } from '@material-ui/data-grid';
 import * as React from 'react';
+import logo from '../logo.png';
 
 const axios = require('axios').default;
 
 export default function EmployeeDashboard() {
   const [orders, setOrders] = React.useState([]);
+  const [orderDetails, setOrderDetails] = React.useState([]);
   const columns = [
     { field: 'id', headerName: 'Order ID', width:150 },
     { field: 'date', headerName: 'Date', width: 250},
     { field: 'firstName', headerName: ' First Name', width:150 },
     { field: 'lastName', headerName: 'Last Name', width:150 },
-    { field: 'addressId', headerName: 'Address', width:150 }
+    { field: 'address', headerName: 'Address', width:400 }
   ];
+
+  async function getOrderDetails(id) {
+    try {
+      const response = await axios.get('http://localhost:3000/orders/' + id.target.value + '/order-items');
+      console.log(response.data);
+      setOrderDetails(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   async function getOrders() {
     try {
@@ -19,15 +32,31 @@ export default function EmployeeDashboard() {
       let tmp = response.data;
       tmp.map(async function (order) {
         const res = await axios.get('http://localhost:3000/customers/' + order['customerId']);
+        const addrRes = await axios.get('http://localhost:3000/addresses/' + order['addressId'])
         Object.keys(res.data).forEach(function(key) {
           if (key !== 'id') {
             order[key] = res.data[key];
           }
         });
         order['date'] = (new Date(order['date'])).toString();
+        let addr = addrRes.data.line1 + ", ";
+        if (addrRes.data.line2) {
+          addr = addr + addrRes.data.line2 + ", ";
+        }
+        addr = addr + addrRes.data.city + " " + addrRes.data.state + " " + addrRes.data.zipCode;
+        console.log(addr);
+        order['address'] = addr;
       });
       setOrders(tmp);
       console.log(tmp);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async function updateOrder(param) {
+    try {
+      const res = await axios.delete('http://localhost:3000/orders/' + param.data.id);
+      getOrders();
     } catch (error) {
       console.log(error);
     }
@@ -38,7 +67,11 @@ export default function EmployeeDashboard() {
 
   return (
     <div style ={{ height: 400, width: '100%' }}>
-      <DataGrid rows={orders} columns={columns} pageSize={5} checkboxSelection />
+      <div ><img className="App-logo" src={logo} class="center" style={{width:"20%",height:"20%"}} alt='logo'/></div>
+      <h3>Pending orders</h3>
+      <DataGrid rows={orders} columns={columns} pageSize={5} onRowSelected={updateOrder} checkboxSelection />
+      <TextField onChange={e => getOrderDetails(e)}/>
+      Order details: {JSON.stringify(orderDetails)}
     </div>
   );
 }
